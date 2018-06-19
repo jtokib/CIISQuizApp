febf9c822605b31a54bb6d85c8ff0acefcae5eb3
jtokib <tokib520@yahoo.com>
2018-06-18 17:20:09 -0700

    Fixed MainActivity.java code for checkboxes so 1 point is awarded only when all the correct boxes are checked. Added ic_launcher_background file to project.

--- app/src/main/java/com/example/android/ciisquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/ciisquizapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.android.ciisquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatRadioButton;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -19,83 +18,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /*
-    * I probably wouldn't have the single function do everything, but it's 11pm and I'm running out of time so in my next version of this app I'll split up the code.
-    * */
     public void submitQuiz(View view) {
 
         //Question 1
-        RadioButton checkQuestionOne = (RadioButton) findViewById(R.id.q1a2);
+        RadioButton checkQuestionOne = findViewById(R.id.q1a2);
         boolean questionOneAnswer = checkQuestionOne.isChecked();
         if(questionOneAnswer) {
             answeredCorrect++;
         }
 
         //Question 2
-        CheckBox checkQuestionTwoA = (CheckBox) findViewById(R.id.q2a1);
-        CheckBox checkQuestionTwoB = (CheckBox) findViewById(R.id.q2a4);
-        boolean questionTwoAnswerA = checkQuestionTwoA.isChecked();
-        boolean questionTwoAnswerB = checkQuestionTwoB.isChecked();
-        if(questionTwoAnswerA) {
-            answeredCorrect++;
-        }
-        if(questionTwoAnswerB) {
+        CheckBox checkQuestionTwoA = findViewById(R.id.q2a1);
+        CheckBox checkQuestionTwoB = findViewById(R.id.q2a2);
+        CheckBox checkQuestionTwoC = findViewById(R.id.q2a3);
+        CheckBox checkQuestionTwoD = findViewById(R.id.q2a4);
+        CheckBox checkQuestionTwoE = findViewById(R.id.q2a5);
+        boolean questionTwoAnswer = checkQuestionTwoA.isChecked() && !checkQuestionTwoB.isChecked() && !checkQuestionTwoC.isChecked() && checkQuestionTwoD.isChecked() && !checkQuestionTwoE.isChecked();
+        if (questionTwoAnswer) {
             answeredCorrect++;
         }
 
         //Question 3
-        RadioButton checkQuestionThree = (RadioButton) findViewById(R.id.q3a3);
+        RadioButton checkQuestionThree = findViewById(R.id.q3a3);
         boolean questionThreeAnswer = checkQuestionThree.isChecked();
         if(questionThreeAnswer) {
             answeredCorrect++;
         }
 
         //Question 4
-        RadioButton checkQuestionFour = (RadioButton) findViewById(R.id.q4a4);
+        RadioButton checkQuestionFour = findViewById(R.id.q4a4);
         boolean questionFourAnswer = checkQuestionFour.isChecked();
         if(questionFourAnswer) {
             answeredCorrect++;
         }
 
         //Question 5
-        CheckBox checkQuestionFiveA = (CheckBox) findViewById(R.id.q5a2);
-        CheckBox checkQuestionFiveB = (CheckBox) findViewById(R.id.q5a4);
-        CheckBox checkQuestionFiveC = (CheckBox) findViewById(R.id.q5a5);
-        CheckBox checkQuestionFiveD = (CheckBox) findViewById(R.id.q5a6);
-        boolean questionFiveAnswerA = checkQuestionFiveA.isChecked();
-        boolean questionFiveAnswerB = checkQuestionFiveB.isChecked();
-        boolean questionFiveAnswerC = checkQuestionFiveC.isChecked();
-        boolean questionFiveAnswerD = checkQuestionFiveD.isChecked();
-        if(questionFiveAnswerA) {
-            answeredCorrect++;
-        }
-        if(questionFiveAnswerB) {
-            answeredCorrect++;
-        }
-        if(questionFiveAnswerC) {
-            answeredCorrect++;
-        }
-        if(questionFiveAnswerD) {
+        CheckBox checkQuestionFiveA = findViewById(R.id.q5a1);
+        CheckBox checkQuestionFiveB = findViewById(R.id.q5a2);
+        CheckBox checkQuestionFiveC = findViewById(R.id.q5a3);
+        CheckBox checkQuestionFiveD = findViewById(R.id.q5a4);
+        CheckBox checkQuestionFiveE = findViewById(R.id.q5a5);
+        CheckBox checkQuestionFiveF = findViewById(R.id.q5a6);
+        
+        boolean questionFiveAnswer = !checkQuestionFiveA.isChecked() && checkQuestionFiveB.isChecked() && !checkQuestionFiveC.isChecked() && checkQuestionFiveD.isChecked() && checkQuestionFiveE.isChecked() && checkQuestionFiveF.isChecked();
+        if (questionFiveAnswer) {
             answeredCorrect++;
         }
 
         //Question 6
-        EditText questionSix = (EditText) findViewById(R.id.q6a1);
+        EditText questionSix = findViewById(R.id.q6a1);
         String questionSixAnswer = questionSix.getText().toString().toUpperCase();
-        if(questionSixAnswer != null && questionSixAnswer.equals("JUDIE WEXLER")) {
+        if (questionSixAnswer.equals("JU" +
+                "DIE WEXLER")) {
             answeredCorrect++;
         }
 
         //Display toast messages
-        if (answeredCorrect == 10) {
+        if (answeredCorrect == 6) {
             Toast.makeText(this, "Congratulations! You Answered Everything Correctly", Toast.LENGTH_LONG).show();
             answeredCorrect = 0;
             return;
-        } else if (answeredCorrect > 7) {
+        } else if (answeredCorrect > 4) {
             Toast.makeText(this, "Good job! You got " + answeredCorrect + " answers right!", Toast.LENGTH_LONG).show();
             answeredCorrect = 0;
             return;
-        } else if (answeredCorrect > 4) {
+        } else if (answeredCorrect > 2) {
             Toast.makeText(this, "Not bad. You got " + answeredCorrect + " answers right. Try learning more about us online.", Toast.LENGTH_LONG).show();
             answeredCorrect = 0;
             return;
